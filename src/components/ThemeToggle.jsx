8c6e07ad6396e7cbbcdb627dc79a6ea1c5d3779e
Darshan kyada <darshankyada02@gmail.com>
2025-05-20 17:22:10 +0530

    index.css in import tailwindCss, animation, utility classes and root block code other changes

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,33 @@
+import { Moon, Sun } from 'lucide-react';
+import React, { useState } from 'react'
+
+const ThemeToggle = () => {
+
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const toggleTheme = () => {
+        if (isDarkMode) {
+            document.documentElement.classList.remove('dark');
+            setIsDarkMode(false)
+        } else {
+            document.documentElement.classList.add('dark');
+            setIsDarkMode(true);
+        }
+    }
+
+    return (
+        <div>
+            <button onClick={toggleTheme}>
+                {
+                    isDarkMode ? (
+                        <Sun className='h-6 w-6 text-yellow-300' />
+                    ) : (
+                        <Moon className='h-6 w-6 text-blue-900' />
+                    )
+                }
+            </button>
+        </div>
+    )
+}
+
+export default ThemeToggle
